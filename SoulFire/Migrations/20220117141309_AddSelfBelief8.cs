b64064baed2bfba5b1bcf2991f7d3f15eac8c39f
developer <kill2survive@yandex.ru>
2022-01-17 19:26:52 +0500

    user answers

---
+++ SoulFire/Migrations/20220117141309_AddSelfBelief8.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoulFire.Migrations
+{
+    public partial class AddSelfBelief8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
